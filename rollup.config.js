d9b24c1a91769be165ac91f369a2a9fbf43987e0
AdeeshaMahalekam <amahlekam@innodata.com>
2025-01-19 22:00:56 +0530

    update build configuration for webp & svg files

--- rollup.config.js
+++ rollup.config.js
@@ -5,6 +5,8 @@ import terser from "@rollup/plugin-terser";
 import dts from "rollup-plugin-dts";
 import postcss from "rollup-plugin-postcss";
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import url from "rollup-plugin-url";
+import svgr from "@svgr/rollup";
 
 const packageJson = require("./package.json");
 
@@ -30,6 +32,14 @@ export default [
       typescript({ tsconfig: "./tsconfig.json" }),
       terser(),
       postcss(),
+      svgr({ include: ["**/*.svg"] }), // Handle svg files
+      url({
+        // Handle image files
+        include: ["**/*.webp", "**/*.png", "**/*.jpg", "**/*.jpeg", "**/*.gif"],
+        limit: 8192, // Inline files smaller than 8KB (adjust as needed)
+        publicPath: "assets/", // Output path for larger assets
+        emitFiles: true,
+      }),
     ],
     external: ["react", "react-dom"],
   },
